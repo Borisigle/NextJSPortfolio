13f04454595d87a2bf339340100570fa14a1bd72
borisigle <borisiglesias1@gmail.com>
2021-12-01 16:39:42 -0300

    fix route

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ function Home({ data }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/projects");
+  const res = await fetch("http://borisiglesias.vercel.com/api/projects");
   const data = await res.json();
 
   return {
