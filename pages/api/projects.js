038ff1e3a2bd18abe0cddc3738a6e39e5632fe85
Borisigle <borisiglesias1@gmail.com>
2021-12-17 00:55:32 -0300

    add responsive nav

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,28 +1,40 @@
-
 export default function projects(req, res) {
-    res.status(200).json([
+  res.status(200).json([
     {
-      "name": "Coin status",
-      "stack": "React, Chakra UI",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et orci tristique, porta neque et, ultricies leo. Vivamus non maximus magna, fringilla commodo enim. ",
-      "github_url": "https://github.com/Borisigle/CryptoStatus",
-      "site_url": "https://coinsstatus.netlify.app/",
-      "image": "",
+      name: "Coin status",
+      stack: "React, Chakra UI",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et orci tristique, porta neque et, ultricies leo. Vivamus non maximus magna, fringilla commodo enim. ",
+      github_url: "https://github.com/Borisigle/CryptoStatus",
+      site_url: "https://coinsstatus.netlify.app/",
+      image: "",
     },
     {
-      "name": "Task It",
-      "stack": "React, Chakra UI",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et orci tristique, porta neque et, ultricies leo. Vivamus non maximus magna, fringilla commodo enim. ",
-      "github_url": "https://github.com/Borisigle/TaskManager",
-      "site_url": "https://taskit.netlify.app/",
-      "image": "",
+      name: "Task It",
+      stack: "React, Chakra UI",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et orci tristique, porta neque et, ultricies leo. Vivamus non maximus magna, fringilla commodo enim. ",
+      github_url: "https://github.com/Borisigle/TaskManager",
+      site_url: "https://taskit.netlify.app/",
+      image: "",
     },
     {
-      "name": "Interval timer",
-      "stack": "React, Chakra UI",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et orci tristique, porta neque et, ultricies leo. Vivamus non maximus magna, fringilla commodo enim. ",
-      "github_url": "https://github.com/Borisigle/Interval-Timer",
-      "site_url": "https://intervaltimerapp.netlify.app/",
-      "image": "",
-    }
-  ])}
+      name: "Interval timer",
+      stack: "React, Chakra UI",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et orci tristique, porta neque et, ultricies leo. Vivamus non maximus magna, fringilla commodo enim. ",
+      github_url: "https://github.com/Borisigle/Interval-Timer",
+      site_url: "https://intervaltimerapp.netlify.app/",
+      image: "",
+    },
+    {
+      name: "Tic tac toe",
+      stack: "React, Chakra UI",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et orci tristique, porta neque et, ultricies leo. Vivamus non maximus magna, fringilla commodo enim. ",
+      github_url: "https://github.com/Borisigle/tictactoe",
+      site_url: "https://tictactoe-ivory.vercel.app/",
+      image: "",
+    },
+  ]);
+}
